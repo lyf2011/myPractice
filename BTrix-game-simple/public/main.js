2ef2b8e2c54c741e7a50b8b0a01f3baa86b6cad4
liyaofeng <liyaofeng@egova.com.cn>
2018-10-18 20:02:07 +0800

    增加initDivs方法---搭建界面

--- BTrix-game-simple/public/main.js
+++ BTrix-game-simple/public/main.js
@@ -3,12 +3,17 @@ const GAME_ROW = 20
 const GAME_COLUMN = 10
 const NEXT_ROW = 4
 const NEXT_COLUMN = 4
+const CELL_WIDTH = 20
 
 export default class Game {
     constructor() {
         // 游戏区数据数组和next数据数组
         this.gameData = []
         this.nextData = []
+
+        // 游戏区的所有div和next区域所有的div
+        this.gameDivs = []
+        this.nextDivs = []
     }
 
     initGame() {
@@ -29,4 +34,22 @@ function initData(row, column) {
 
     return arr_2D
 }
+
+function initDivs(row, column) {
+    let areaDivs = []    //整个面的div数组，二维的
+    for (var i = 0; i < row; i++) {
+        let lineDivs = [] //一行的div数组
+        for (var j = 0; j < column; j++) {
+            let newDiv = document.createElement("div")
+            newDiv.className = "none"
+            newDiv.style.top = i * CELL_WIDTH + "px"
+            newDiv.style.left = j * CELL_WIDTH + "px"
+            lineDivs.push(newDiv)
+        }
+        areaDivs.push(lineDivs)
+    }
+
+    return lineDivs
+}
+
 
